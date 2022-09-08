cb25afda0fd1e10bbb0dc8b725ec145973f294a6
Will-H007 <wkayi076@gmail.com>
2022-09-08 16:41:53 +0800

    bollinger strategy

--- range_trading.py
+++ range_trading.py
@@ -23,6 +23,19 @@ logic() function:
     Output: none, but the account object will be modified on each call
 '''
 
+def risk_management(account):
+    # safety_percentage = 1
+    if account.buying_power > 10000:
+        safety_percentage = 0.5
+    elif account.buying_power > 5000 and account.buying_power <= 10000:
+        safety_percentage = 1
+    elif 3500 < account.buying_power and account.buying_power <= 5000:
+        safety_percentage = 0.2
+    else:
+        safety_percentage = 0
+    return safety_percentage
+
+
 def logic(account, lookback): # Logic function to be used for each time interval in backtest 
     
     today = len(lookback)-1
@@ -46,24 +59,54 @@ def logic(account, lookback): # Logic function to be used for each time interval
     
     '''
     #Check every day (64)
-    # if today % period == 0:
+    if today % period == 0 and today >=  training_period*2:
 
-    #     fig, ax = plt.subplots()
+       
+        safety_percentage = risk_management(account)
 
-    #     # Plot the average true range
-    #     lookback["atr"].plot(ax=ax)
-    #     lookback['close'].plot(ax=ax, secondary_y=True, alpha=0.3)
+        #Range market
         
-    #     # Plot the Bollinger Bands
-    #     ax = lookback[['close', 'BOLU', 'BOLD']].plot(color=['blue', 'orange', 'yellow'])
-    #     ax.fill_between(lookback.index, lookback['BOLD'], lookback['BOLU'], facecolor='orange', alpha=0.1)
+        if(lookback['close'][today] < lookback['BOLD'][today]) and lookback['percent_b'][today-64:today].mean() < 0.3: # If current price is below lower Bollinger Band, enter a long position
+            
+            for position in account.positions: # Close all current positions
+                account.close_position(position, 1, lookback['close'][today])
+            if(account.buying_power > 0):
+                account.enter_position('long',account.buying_power * safety_percentage, lookback['close'][today]) # Enter a long position
+
+        elif(lookback['close'][today] > lookback['BOLU'][today]) and lookback['percent_b'][today-64:today].mean() < 0.3: # If today's price is above the upper Bollinger Band, enter a short position
+            for position in account.positions: # Close all current positions
+                account.close_position(position, 1, lookback['close'][today])
+            if(account.buying_power > 0):
+                account.enter_position('short', account.buying_power * safety_percentage, lookback['close'][today]) # Enter a short position
+
+
+        #Trending market
+
+        if lookback['Trend'][today-20:today].sum() > 0 and lookback['MA-TP'][today-20:today].mean() <  lookback['close'][today-20:today].mean() and lookback['close'][today] > lookback['BOLU'][today]:
+            for position in account.positions: # Close all current positions
+                account.close_position(position, 1, lookback['close'][today])
+            if(account.buying_power > 0):
+                account.enter_position('long',account.buying_power * safety_percentage, lookback['close'][today]) # Enter a long position
+
+        elif lookback['Trend'][today-20:today].sum() < 0 and lookback['MA-TP'][today-20:today].mean() >  lookback['close'][today-20:today].mean() and lookback['close'][today] < lookback['BOLD'][today]:
+            for position in account.positions: # Close all current positions
+                account.close_position(position, 1, lookback['close'][today])
+            if(account.buying_power > 0):
+                account.enter_position('short', account.buying_power * safety_percentage, lookback['close'][today]) # Enter a short position
+
+
+
+
+
+
+        # Plot the Bollinger Bands
+   
+        # ax = plt.subplots()
+        # ax = lookback[['close', 'MA-TP', 'BOLU', 'BOLD']].plot(color=['blue','green' ,'orange', 'yellow'])
+        # ax.fill_between(lookback.index, lookback['BOLD'], lookback['BOLU'], facecolor='orange', alpha=0.1)
     
 
-    #     plt.show()
-        # if(account.buying_power > 0):
-        #     account.enter_position('short', account.buying_power, lookback['close'][today])
-
-
+        # plt.show()
 
 '''
 preprocess_data() function:
@@ -87,10 +130,12 @@ def preprocess_data(list_of_stocks):
 
         # Bollinger Bands
         df['TP'] = (df['close'] + df['low'] + df['high'])/3
-        df['std'] = standard_deviations
+        df['std'] = df['TP'].rolling(training_period).std()
         df['MA-TP'] = df['TP'].rolling(training_period).mean()
         df['BOLU'] = df['MA-TP'] + 2*df['std']
         df['BOLD'] = df['MA-TP'] - 2*df['std']
+        df['Trend'] = df['MA-TP'].pct_change()
+        df["percent_b"] = (df['close'] - df['BOLU']) / (df['BOLD'] - df['BOLU'])
         print(df.describe().transpose())
 
         #Average True Range
@@ -112,8 +157,8 @@ def preprocess_data(list_of_stocks):
 
 
 if __name__ == "__main__":
-    # list_of_stocks = ["TSLA_2020-03-01_2022-01-20_1min"] 
-    list_of_stocks = ["TSLA_2020-03-09_2022-01-28_15min"] # List of stock data csv's to be tested, located in "data/" folder  # "AAPL_2020-03-24_2022-02-12_15min"
+    # list_of_stocks = ["TSLA_2020-03-01_2022-01-20_1min" ] 
+    list_of_stocks = ["AAPL_2020-03-24_2022-02-12_15min", "TSLA_2020-03-09_2022-01-28_15min", "TSLA_2020-03-01_2022-01-20_1min"] # List of stock data csv's to be tested, located in "data/" folder  # "AAPL_2020-03-24_2022-02-12_15min"
     list_of_stocks_proccessed = preprocess_data(list_of_stocks) # Preprocess the data
     results = tester.test_array(list_of_stocks_proccessed, logic, chart=True) # Run backtest on list of stocks using the logic function
 
